8605d9587cdf72a910f806c05a8779800e36e4b5
pavel <prohorenkopavel74@gmail.com>
2022-05-24 16:57:13 +0500

    add user table

---
+++ app/models/user.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+import sqlalchemy_utils
+
+from app.db.metadata import postgres_metadata
+
+users = sqlalchemy.Table(
+    "users",
+    postgres_metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True, index=True),
+    sqlalchemy.Column(
+        "email", sqlalchemy_utils.EmailType, unique=True, index=True, nullable=False
+    ),
+    sqlalchemy.Column("hashed_password", sqlalchemy.String, nullable=False),
+    sqlalchemy.Column("first_name", sqlalchemy.String),
+    sqlalchemy.Column("last_name", sqlalchemy.String),
+    sqlalchemy.Column("is_active", sqlalchemy.Boolean, default=True),
+    sqlalchemy.Column("is_superuser", sqlalchemy.Boolean, default=False),
+)
